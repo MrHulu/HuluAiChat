e69ad5a6c9b619a9b27cbc0fd0a3184be30dd97b
MrHulu <62984804+MrHulu@users.noreply.github.com>
2026-03-16 15:05:55 +0000

    chore(website): update version to v3.71.0 [skip ci]

--- website/src/lib/version.ts
+++ website/src/lib/version.ts
@@ -5,6 +5,6 @@
  * DO NOT manually edit this file - it will be overwritten during releases.
  */
 
-export const VERSION = "3.69.0";
+export const VERSION = "3.71.0";
 
 export const RELEASE_NOTES = "New features and improvements";
